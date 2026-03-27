621e76a142486eb51f49fdf1ea372b88789a5093
Your Name <you@example.com>
2026-03-27 16:57:43 +0800

    Done playing and updating the 03-flexible-types.ts file

---
+++ 03-flexible-types.ts
@@ -0,0 +1,19 @@
+let age: any = 31;
+
+age = '31';
+console.log(age, typeof age);
+
+age = true;
+console.log(age, typeof age);
+
+age = {};
+console.log(age, typeof age);
+
+age = [];
+console.log(age, typeof age);
+
+age = null;
+console.log(age, typeof age);
+
+age = undefined;
+console.log(age, typeof age);
