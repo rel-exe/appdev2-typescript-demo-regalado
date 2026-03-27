6b3a76e2c5c0e34853c31fa61108f39c1bef7594
Your Name <you@example.com>
2026-03-27 15:43:32 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,12 @@
-let userName1 = "Elmer";
+let userName1 = "russelenia";
+let userFavorite = "mushrooms"
+let userAge = 20
 
+console.log(`Favorite food: ${userFavorite}`);
 console.log(typeof userName1)
+
+console.log(`Favorite food: ${userFavorite}`);
+console.log(typeof userFavorite)
+
+console.log(`Age: ${userAge}`);
+console.log(typeof userAge)
